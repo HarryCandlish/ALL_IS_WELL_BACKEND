3ea4d44de9d5b4966c7bc92471d1289ab82d702b
HarryCandlish <hcnd90@gmail.com>
2019-07-10 09:22:28 +1200

    created routes and db functions

--- server.js
+++ server.js
@@ -8,5 +8,6 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "./public")));
 
 app.use("/api/philosophy", require("./routes/philosophy"));
+app.use("/api/products", require("./routes/products"));
 
 module.exports = app;
